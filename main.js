729efa06c57f37bbd54c15d026d565ae2db8199c
Arun8008 <117275772+Arun8008@users.noreply.github.com>
2023-03-18 15:21:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let year=prompt("Enter The Year");
 
 if((year%4==0) && (year%400==0)){
-   document.write=`${year} Year Leap Year`;
+   document.write(`${year} Leap Year`);
 }
 else{
    document.write(`${year} Normal Year`);
